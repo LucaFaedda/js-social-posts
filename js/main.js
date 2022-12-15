98ad3c67db5cbf664dc8daf7bf4425a99c120d33
LucaFaedda <lucakarl91@gmail.com>
2022-12-15 15:48:25 +0100

    fatto variabile che si collega al pulsante

--- js/main.js
+++ js/main.js
@@ -57,42 +57,50 @@ const posts = [
 ];
 
 
+
+
 let post = '';
 
 posts.forEach(elem => {
     post += `<div class="post">
     <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${elem.author.image}" alt="immagine-profilo">                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${elem.author.name}</div>
-                <div class="post-meta__time">${elem.created}</div>
-            </div>                    
-        </div>
+    <div class="post-meta">                    
+    <div class="post-meta__icon">
+    <img class="profile-pic" src="${elem.author.image}" alt="immagine-profilo">                    
+    </div>
+    <div class="post-meta__data">
+    <div class="post-meta__author">${elem.author.name}</div>
+    <div class="post-meta__time">${elem.created}</div>
+    </div>                    
+    </div>
     </div>
     <div class="post__text">${elem.content}</div>
     <div class="post__image">
-        <img src="${elem.media}" alt="">
+    <img src="${elem.media}" alt="">
     </div>
     <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
-            </div>
-        </div> 
+    <div class="likes js-likes">
+    <div class="likes__cta">
+    <a class="like-button  js-like-button" href="#" data-postid="1">
+    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    <span class="like-button__label">Mi Piace</span>
+    </a>
+    </div>
+    <div class="likes__counter">
+    Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
+    </div>
+    </div> 
     </div>            
-</div> `
-return post
+    </div> `
+    return elem
 });
+
+
+const miPiace = document.querySelector('.like-button__label')
+console.log(miPiace)
 
 const postContain = document.querySelector('.posts-list');
 console.log(postContain)
 postContain.innerHTML += post
+
+
