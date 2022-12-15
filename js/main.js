487127b3c4dd0d697f8e81767fff7682d9958e77
LucaFaedda <lucakarl91@gmail.com>
2022-12-15 15:12:19 +0100

    millestone 2 completato

--- js/main.js
+++ js/main.js
@@ -55,3 +55,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+let post = '';
+
+posts.forEach(elem => {
+    post += `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${elem.author.image}" alt="immagine-profilo">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${elem.author.name}</div>
+                <div class="post-meta__time">${elem.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${elem.content}</div>
+    <div class="post__image">
+        <img src="${elem.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div> `
+return post
+});
+
+const postContain = document.querySelector('.posts-list');
+console.log(postContain)
+postContain.innerHTML += post
